775054901c9644a36a1c7f0fac125bb5d9f153bb
YunHaaaa <qlxqlrt2012@gmail.com>
2024-02-15 13:44:58 +0900

    Feat: change network to tensorflow

--- network/atloc.py
+++ network/atloc.py
@@ -1,38 +1,34 @@
 import time
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.nn.init
+import tensorflow as tf
+from tensorflow.keras import layers
 from network.att import AttentionBlock
 
-class FourDirectionalLSTM(nn.Module):
+class FourDirectionalLSTM(tf.keras.layers.Layer):
     def __init__(self, seq_size, origin_feat_size, hidden_size):
         super(FourDirectionalLSTM, self).__init__()
         self.feat_size = origin_feat_size // seq_size
         self.seq_size = seq_size
         self.hidden_size = hidden_size
-        self.lstm_rightleft = nn.LSTM(self.feat_size, self.hidden_size, batch_first=True, bidirectional=True)
-        self.lstm_downup = nn.LSTM(self.seq_size, self.hidden_size, batch_first=True, bidirectional=True)
+        self.lstm_rightleft = layers.Bidirectional(layers.LSTM(self.hidden_size, return_sequences=True))
+        self.lstm_downup = layers.Bidirectional(layers.LSTM(self.hidden_size, return_sequences=True))
 
-    def init_hidden_(self, batch_size, device):
-        return (torch.randn(2, batch_size, self.hidden_size).to(device),
-                torch.randn(2, batch_size, self.hidden_size).to(device))
+    def build(self, input_shape):
+        # Implement build method if needed for custom layer initialization
+        pass
 
-    def forward(self, x):
-        batch_size = x.size(0)
-        x_rightleft = x.view(batch_size, self.seq_size, self.feat_size)
-        x_downup = x_rightleft.transpose(1, 2)
-        hidden_rightleft = self.init_hidden_(batch_size, x.device)
-        hidden_downup = self.init_hidden_(batch_size, x.device)
-        _, (hidden_state_lr, _) = self.lstm_rightleft(x_rightleft, hidden_rightleft)
-        _, (hidden_state_ud, _) = self.lstm_downup(x_downup, hidden_downup)
-        hlr_fw = hidden_state_lr[0, :, :]
-        hlr_bw = hidden_state_lr[1, :, :]
-        hud_fw = hidden_state_ud[0, :, :]
-        hud_bw = hidden_state_ud[1, :, :]
-        return torch.cat([hlr_fw, hlr_bw, hud_fw, hud_bw], dim=1)
+    def call(self, x):
+        batch_size = tf.shape(x)[0]
+        x_rightleft = tf.reshape(x, (batch_size, self.seq_size, self.feat_size))
+        x_downup = tf.transpose(x_rightleft, perm=[0, 2, 1])
+        hidden_rightleft = self.lstm_rightleft(x_rightleft)
+        hidden_downup = self.lstm_downup(x_downup)
+        hlr_fw = hidden_rightleft[:, :, :self.hidden_size]
+        hlr_bw = hidden_rightleft[:, :, self.hidden_size:]
+        hud_fw = hidden_downup[:, :, :self.hidden_size]
+        hud_bw = hidden_downup[:, :, self.hidden_size:]
+        return tf.concat([hlr_fw[:, -1, :], hlr_bw[:, -1, :], hud_fw[:, -1, :], hud_bw[:, -1, :]], axis=1)
 
-class AtLoc(nn.Module):
+class AtLoc(tf.keras.Model):
     def __init__(self, feature_extractor, droprate=0.5, pretrained=True, feat_dim=2048, lstm=False):
         super(AtLoc, self).__init__()
         self.droprate = droprate
@@ -41,57 +37,53 @@ class AtLoc(nn.Module):
 
         # replace the last FC layer in feature extractor
         self.feature_extractor = feature_extractor
-        self.feature_extractor.avgpool = nn.AdaptiveAvgPool2d(1)
-        fe_out_planes = self.feature_extractor.fc.in_features
-        self.feature_extractor.fc = nn.Linear(fe_out_planes, feat_dim)
+        self.feature_extractor.layers[-1] = layers.GlobalAveragePooling2D()
+        fe_out_planes = self.feature_extractor.layers[-1].output_shape[-1]
+        self.feature_extractor.layers[-1] = layers.Dense(feat_dim)
 
         if self.lstm:
             self.lstm4dir = FourDirectionalLSTM(seq_size=32, origin_feat_size=feat_dim, hidden_size=256)
-            self.fc_xy = nn.Linear(feat_dim // 2, 2)
-            self.fc_yaw = nn.Linear(feat_dim // 2, 1)
+            self.fc_xy = layers.Dense(2)
+            self.fc_yaw = layers.Dense(1)
         else:
             self.att = AttentionBlock(feat_dim)
-            self.fc_xy = nn.Linear(feat_dim, 2)
-            self.fc_yaw = nn.Linear(feat_dim, 1)
+            self.fc_xy = layers.Dense(2)
+            self.fc_yaw = layers.Dense(1)
 
         # initialize
         if pretrained:
-            init_modules = [self.feature_extractor.fc, self.fc_xy, self.fc_yaw]
+            init_modules = [self.feature_extractor.layers[-1], self.fc_xy, self.fc_yaw]
         else:
-            init_modules = self.modules()
+            init_modules = self.submodules()
 
         for m in init_modules:
-            if isinstance(m, nn.Conv2d) or isinstance(m, nn.Linear):
-                nn.init.kaiming_normal_(m.weight.data)
+            if isinstance(m, layers.Conv2D) or isinstance(m, layers.Dense):
+                m.build((None, fe_out_planes))
+                m.kernel_initializer = 'glorot_normal'
                 if m.bias is not None:
-                    nn.init.constant_(m.bias.data, 0)
+                    m.bias_initializer = 'zeros'
 
-    def forward(self, x):
+    def call(self, x):
         tstart = time.time()
 
         x = self.feature_extractor(x)
-        x = F.relu(x)
+        x = tf.nn.relu(x)
 
         if self.lstm:
             x = self.lstm4dir(x)
         else:
-            x = self.att(x.view(x.size(0), -1))
+            x = self.att(x.view(tf.shape(x)[0], -1))
 
         if self.droprate > 0:
-            x = F.dropout(x, p=self.droprate)
+            x = tf.nn.dropout(x, rate=self.droprate)
 
         xy = self.fc_xy(x)
         yaw = self.fc_yaw(x)
 
         self.inference_time = time.time() - tstart
 
-        return torch.cat((xy, yaw), 1)
-
+        return tf.concat([xy, yaw], axis=1)
 
     def get_last_inference_time(self, with_nms=True):
-        """
-        Returns a tuple containing most recent inference and NMS time
-        """
         res = [self.inference_time]
-        
         return res
--- network/att.py
+++ network/att.py
@@ -1,31 +1,34 @@
-import torch
-from torch import nn
-from torch.nn import functional as F
+import tensorflow as tf
+from tensorflow.keras import layers
 
-
-class AttentionBlock(nn.Module):
+class AttentionBlock(tf.keras.layers.Layer):
     def __init__(self, in_channels):
         super(AttentionBlock, self).__init__()
-        self.g = nn.Linear(in_channels, in_channels // 8)
-        self.theta = nn.Linear(in_channels, in_channels // 8)
-        self.phi = nn.Linear(in_channels, in_channels // 8)
+        self.g = layers.Dense(in_channels // 8)
+        self.theta = layers.Dense(in_channels // 8)
+        self.phi = layers.Dense(in_channels // 8)
+        self.W = layers.Dense(in_channels)
 
-        self.W = nn.Linear(in_channels // 8, in_channels)
+    def call(self, x):
+        batch_size = tf.shape(x)[0]
+        out_channels = x.shape[-1]
 
-    def forward(self, x):
-        batch_size = x.size(0)
-        out_channels = x.size(1)
+        g_x = self.g(x)
+        g_x = tf.reshape(g_x, (batch_size, out_channels // 8, 1))
 
-        g_x = self.g(x).view(batch_size, out_channels // 8, 1)
+        theta_x = self.theta(x)
+        theta_x = tf.reshape(theta_x, (batch_size, out_channels // 8, 1))
+        theta_x = tf.transpose(theta_x, perm=[0, 2, 1])
 
-        theta_x = self.theta(x).view(batch_size, out_channels // 8, 1)
-        theta_x = theta_x.permute(0, 2, 1)
-        phi_x = self.phi(x).view(batch_size, out_channels // 8, 1)
-        f = torch.matmul(phi_x, theta_x)
-        f_div_C = F.softmax(f, dim=-1)
+        phi_x = self.phi(x)
+        phi_x = tf.reshape(phi_x, (batch_size, out_channels // 8, 1))
 
-        y = torch.matmul(f_div_C, g_x)
-        y = y.view(batch_size, out_channels // 8)
+        f = tf.matmul(phi_x, theta_x)
+        f_div_C = tf.nn.softmax(f, axis=-1)
+
+        y = tf.matmul(f_div_C, g_x)
+        y = tf.reshape(y, (batch_size, out_channels // 8))
         W_y = self.W(y)
         z = W_y + x
+
         return z
